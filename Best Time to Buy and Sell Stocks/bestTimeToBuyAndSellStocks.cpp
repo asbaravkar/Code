2441f47348f92b76f9b7a8033033f876285efa18
Aditya Baravkar <42476422+asbaravkar@users.noreply.github.com>
2020-10-30 09:11:58 +0530

    121. Best Time to Buy and Sell Stock (LeetCode)

---
+++ Best Time to Buy and Sell Stocks/bestTimeToBuyAndSellStocks.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int mini = INT_MAX;
+        int maxProfit = 0;
+        for(int i=0; i<prices.size(); ++i){
+            mini = min(mini, prices[i]);
+            maxProfit = max(maxProfit, prices[i]-mini);
+        }
+        return maxProfit;
+    }
+};
