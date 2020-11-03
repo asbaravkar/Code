caea0027176bcebbb41185e6a0e345dc5c6803e1
Aditya Baravkar <42476422+asbaravkar@users.noreply.github.com>
2020-11-04 00:14:55 +0530

    1446. Consecutive Characters (LeetCode)

---
+++ Consecutive Characters/consecutiveCharacters.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int maxPower(string s) {
+        int max_count = 1;
+        int count = 1;
+        
+        char prev  = s[0];
+        for(int i=1; i<s.length(); ++i){
+            if(s[i] == prev){
+                count++;
+              max_count = max(max_count, count);  
+            } 
+            else{
+                
+                count = 1;
+                prev = s[i];
+            }
+        }
+        return max_count;
+    }
+};
